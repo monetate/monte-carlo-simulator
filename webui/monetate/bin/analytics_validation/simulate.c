a5eee69c441e7c84f37b19a6621eee3bb2090b3c
Jeffrey J. Persch <jjpersch@monetate.com>
2013-05-14 12:35:19 -0400

    Require three args.
    Allowing single group by testing.

--- webui/monetate/bin/analytics_validation/simulate.c
+++ webui/monetate/bin/analytics_validation/simulate.c
@@ -146,7 +146,7 @@ simulate(FILE* in, FILE* out, double* group_weights, int groups, int simulations
 int
 main(int argc, char** argv)
 {
-    if (argc < 2) {
+    if (argc < 3) {
         fprintf(stderr, "Usage: simulate iterations weight0 weight1 ...\n");
         exit(2);
     }
